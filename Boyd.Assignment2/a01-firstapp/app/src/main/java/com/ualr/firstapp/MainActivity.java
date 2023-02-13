ff14210e9e949e702cee5dac5d86adf3e531cf17
LoamySand <laneeboyd@gmail.com>
2023-02-13 17:08:08 -0600

    Add solution to assignment 2

---
+++ Boyd.Assignment2/a01-firstapp/app/src/main/java/com/ualr/firstapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ualr.firstapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ualr.firstapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View rootView = mBinding.getRoot();
+        setContentView(rootView);
+    }
+
+    public void showTextMessage(View view) {
+        if (!mBinding.userInputET.getText().toString().isEmpty()) {
+            mBinding.userMsgTV.setText(mBinding.userInputET.getText().toString());
+        }
+    }
+
+    public void cleanTextField(View view) {
+        mBinding.userInputET.setText("");
+    }
+}
